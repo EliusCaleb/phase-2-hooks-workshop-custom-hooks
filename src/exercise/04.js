a34e827174242988883efb57129c1057962bf5b5
ELIUS <eliuswanyoro@gmail.com>
2022-09-30 19:49:07 +0300

    Done

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -10,19 +10,30 @@ export function useLocalStorage(key, initialValue) {
     ✅ in this hook, use the useState hook. For the initial value for state:
     use the value saved in localStorage OR the initialValue from the function parameters 
   */
-
+     //const [state,setState]=useState(initialValue)
   /* 
    ✅ write a useEffect hook 
    in the useEffect, when state is updated, save the state to localStorage
    don't forget the dependencies array!
   */
-  useEffect(() => {});
+  useEffect(() => {
+    const storedValue = getLocalStorageValue(key);
+  const [state, setState] = useState(storedValue || initialValue);
+
+  useEffect(() => {
+    setLocalStorageValue(key, state);
+  }, [key, state]);
+
+  return [state, setState];
+  },[]);
 
   /* 
    ✅ return the same interface as useState:
    an array with state and a setState function
   */
   // 👀 return [state, setState]
+
+  return [state,setState]
 }
 
 function Form() {
